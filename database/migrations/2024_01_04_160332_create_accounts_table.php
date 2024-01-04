ebf2cf0a6bd5321b215e8c5802d15049e9a9d1ea
Vitor Nunes da Silva <vitor1nuness@gmail.com>
2024-01-04 13:15:40 -0300

    feat created table accounts and migrate

--- database/migrations/2024_01_04_160332_create_accounts_table.php
+++ database/migrations/2024_01_04_160332_create_accounts_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('accounts', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->string('banco');
+            $table->enum('tipo', ['corrente', 'poupanca', 'credito', 'outro']);
+            $table->float('saldo', 10, 2)->nullable();
+            $table->integer('vencimento')->nullable();
+            $table->foreignId('user_id')->constrained('users');
             $table->timestamps();
         });
     }
